8450e5d0aa6c2fe8f2270617065dadecbdcdec81
CAUCHY maxime <macauchy@student.42.fr>
2025-07-07 15:52:07 +0200

    removed raycasting call that are not needed, flickering ????

--- srcs/pause_menu.c
+++ srcs/pause_menu.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pause_menu.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lenygarcia <lenygarcia@student.42.fr>      +#+  +:+       +#+        */
+/*   By: macauchy <macauchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 13:42:11 by lengarci          #+#    #+#             */
-/*   Updated: 2025/07/04 20:47:45 by lenygarcia       ###   ########.fr       */
+/*   Updated: 2025/07/07 15:51:02 by macauchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void	pause_menu(t_game *game)
 	{
 		game->menu_mode = 4;
 		mouse_hide(game);
-		raycasting(game);
+		// raycasting(game);
 		return ;
 	}
 	else if (game->menu_mode == 4)
